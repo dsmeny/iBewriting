aee7084861ad58e2de872bc4cbbef80bbaddcbda
Bradley Rubin <bradleyrubin@Bradleys-Mac-mini.local>
2023-04-02 09:58:25 -0400

    redploy to vercel

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,16 +1,43 @@
-import React from "react";
+import { useState, useEffect, useRef } from "react";
+import { FcExpand } from "react-icons/fc";
 import Button from "./Button";
 
 const Card = ({ id, message, deleteHandler }) => {
+  const [expandable, setExpandable] = useState(false);
+
+  const textRef = useRef();
+
+  useEffect(() => {
+    const target = textRef.current;
+    if (target) {
+      const clientHt = (target as HTMLElement).clientHeight;
+      const scrollHt = (target as HTMLElement).scrollHeight;
+      if (scrollHt > clientHt) {
+        setExpandable(true);
+      }
+    }
+  }, []);
+
   const deleteCard = () => {
     deleteHandler(id);
   };
 
+  const expandHandler = () => {
+    // setExpandable(false);
+    const target = textRef.current;
+    (target as HTMLElement).classList.add("readable");
+  };
+
   return (
     <div className="outer-c">
-      <div className="outer-c">
-        <p>{message}</p>
+      <div className="inner-c">
         <Button text={"del"} id={id} onClick={deleteCard} />
+        <p className="cutoff-text" ref={textRef}>
+          {message}
+        </p>
+        {expandable && (
+          <FcExpand className="expandable-icon" onClick={expandHandler} />
+        )}
       </div>
     </div>
   );
