34aa6c8c780ea3d5975edee4df283f8a7fe64323
Bradley Rubin <bradleyrubin@Bradleys-Mac-mini.local>
2023-04-11 19:31:47 -0400

    changes to folder structure

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,7 @@
-import path from "path";
 import { defineConfig } from "vite";
-import backend from "./backend/server.js";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   root: "src",
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:3002",
-        changeOrigin: true,
-      },
-    },
-    open: true,
-  },
-  build: {
-    outDir: "dist",
-    assetsDir: "assets",
-    sourcemap: true,
-  },
-  middleware: (app) => {
-    app.use(backend);
-  },
 });
