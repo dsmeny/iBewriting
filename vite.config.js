8d44ce77318b5565e307f74be228e29799ac07b2
Bradley Rubin <bradleyrubin@Bradleys-Mac-mini.local>
2024-05-03 11:13:36 -0400

    vite config

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   root: "src",
-  build: {
-    outDir: "../dist",
-  },
+  base: "./",
   server: {
     open: true,
   },
